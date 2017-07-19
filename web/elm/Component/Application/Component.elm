df70a68a904d3032c21cd0972b12e1772c1680a2
Tomas Korcak <korczis@gmail.com>
2017-07-19 21:57:12 +0200

    Cleaning

--- web/elm/Component/Application/Component.elm
+++ web/elm/Component/Application/Component.elm
@@ -1,1 +1,7 @@
 module Component.Application.Component exposing(..)
+
+import Component.Application.Init exposing(init)
+import Component.Application.Update exposing(update)
+import Component.Application.View exposing(view)
+import Component.Application.Subscription exposing(subscriptions)
+
--- web/elm/Data/Session.elm
+++ web/elm/Data/Session.elm
@@ -1,11 +1,8 @@
 module Data.Session exposing (Session)
 
 import Phoenix.Socket
-import Phoenix.Channel
-import Phoenix.Push
 
 import Data.User as User exposing (User)
-import Util exposing ((=>))
 import Msg
 
 type alias Session =
@@ -13,12 +10,3 @@ type alias Session =
     , socket: Phoenix.Socket.Socket Msg.Msg
     }
 
-
---attempt : String -> (AuthToken -> Cmd msg) -> Session -> ( List String, Cmd msg )
---attempt attemptedAction toCmd session =
---    case Maybe.map .token session.user of
---        Nothing ->
---            [ "You have been signed out. Please sign back in to " ++ attemptedAction ++ "." ] => Cmd.none
---
---        Just token ->
---            [] => toCmd token
