9dd7b8175d94745ece58d519131d34a25f0bdbcc
Tomas Korcak <korczis@gmail.com>
2017-07-19 21:50:06 +0200

    Add postgrex dependency

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule WebSpa.Mixfile do
+    defmodule WebSpa.Mixfile do
   use Mix.Project
 
   def project do
@@ -18,13 +18,13 @@ defmodule WebSpa.Mixfile do
   def application do
     [mod: {WebSpa, []},
      applications: [
+        :cowboy,
+        :gettext,
+        :logger,
+        :oauth,
         :phoenix,
         :phoenix_pubsub,
         :phoenix_html,
-        :cowboy,
-        :logger,
-        :gettext,
-        :oauth,
         :ueberauth_facebook,
         :ueberauth_google,
         :ueberauth_github,
@@ -42,17 +42,19 @@ defmodule WebSpa.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.4"},
+    [{:ecto, "~> 2.1"},
+     {:cors_plug, "~> 1.3"},
+     {:cowboy, "~> 1.0"},
+     {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+     {:exfmt, github: "lpil/exfmt", only: [:dev, :test], runtime: false},
+     {:gettext, "~> 0.11"},
+     {:oauth, github: "tim/erlang-oauth"},
+     {:phoenix, "~> 1.2.4"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:cors_plug, "~> 1.3"},
-     {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
-     {:exfmt, github: "lpil/exfmt", only: [:dev, :test], runtime: false},
+     {:postgrex, "~> 0.13.3"},
      {:ueberauth, "~> 0.4"},
-     {:oauth, github: "tim/erlang-oauth"},
      {:ueberauth_facebook, "~> 0.6"},
      {:ueberauth_github, "~> 0.4"},
      {:ueberauth_google, "~> 0.5"},
