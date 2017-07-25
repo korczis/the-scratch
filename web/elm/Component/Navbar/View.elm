19f28e5fe2b8d0b6fa0cc5a370342696c81c753d
Tomas Korcak <korczis@gmail.com>
2017-07-25 16:58:06 +0200

    Conditional sigin/signup and signout

--- web/elm/Component/Navbar/View.elm
+++ web/elm/Component/Navbar/View.elm
@@ -21,46 +21,46 @@ import Component.Stats.View
 
 view : Model -> Html Msg.Msg
 view model =
-    div []
-        [ Navbar.config Msg.NavbarMsg
-            |> Navbar.withAnimation
-            |> Navbar.brand
-                [ href "/#/" ]
-                [ img
-                    [ src (Assets.path <| Assets.net)
-                    , class "d-inline-block"
-                    , style
-                        [ ( "width", "30px" )
-                        , ( "margin-right", "15px" )
-                        ]
-                    ]
-                    []
-                , text "Web SPA"
-                ]
-            |> Navbar.items
-                [ Navbar.itemLink [ href "/#/map" ] [ text "Map" ]
-                , Navbar.itemLink [ href "/#/auth/signin" ] [ text "Sign in" ]
-                , Navbar.itemLink [ href "/#/auth/signup" ] [ text "Sign up" ]
-                ]
-            |> Navbar.customItems
+    let
+        signout = case model.session.user of
+            Just _ ->
                 [ Navbar.formItem [ action "/auth/signout", method "post" ]
                     [ Button.button [ Button.attrs [ onClick Msg.SignOut, href "javascript:void(0);" ] ] [ text "Sign out" ]
                     , input [ Html.Attributes.attribute "type" "hidden", name "_csrf_token", value model.flags.csrf ] []
                     , Component.Stats.View.view
                     ]
                 ]
---            |> Navbar.customItems
---                -- Add custom items
---                [ Navbar.formItem []
---                    [ Input.text [ Input.attrs <| [ placeholder "enter" ] ]
---                    , Button.button
---                        [ Button.success
---                        , Button.attrs [ class "ml-sm-2" ]
---                        ]
---                        [ text "Search" ]
---                    ]
---                , Navbar.textItem [ class "muted ml-sm-2" ] [ text "Sign in" ]
---                , Navbar.textItem [ class "muted ml-sm-2" ] [ text "Sign up" ]
---                ]
-            |> Navbar.view model.navbar.state
-        ]
+            Nothing ->
+                []
+
+        navbarItems = case model.session.user of
+            Just _ ->
+                [ Navbar.itemLink [ href "/#/map" ] [ text "Map" ]
+                ]
+            Nothing ->
+                [ Navbar.itemLink [ href "/#/auth/signin" ] [ text "Sign in" ]
+                , Navbar.itemLink [ href "/#/auth/signup" ] [ text "Sign up" ]
+                ]
+
+        customItems = signout
+    in
+        div []
+            [ Navbar.config Msg.NavbarMsg
+                |> Navbar.withAnimation
+                |> Navbar.brand
+                    [ href "/#/" ]
+                    [ img
+                        [ src (Assets.path <| Assets.net)
+                        , class "d-inline-block"
+                        , style
+                            [ ( "width", "30px" )
+                            , ( "margin-right", "15px" )
+                            ]
+                        ]
+                        []
+                    , text "Web SPA"
+                    ]
+                |> Navbar.items navbarItems
+                |> Navbar.customItems customItems
+                |> Navbar.view model.navbar.state
+            ]
