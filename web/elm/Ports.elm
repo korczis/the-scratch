f3afd908d695dce8e83f8a5d5fd8765c60759af0
Tomas Korcak <korczis@gmail.com>
2017-07-25 16:27:45 +0200

    Add sample toJS function

--- web/elm/Ports.elm
+++ web/elm/Ports.elm
@@ -1,7 +1,9 @@
-port module Ports exposing (onSessionChange, storeSession)
+port module Ports exposing (onSessionChange, storeSession, toJs)
 
 import Json.Encode exposing (Value)
 
 port storeSession : Maybe String -> Cmd msg
 
 port onSessionChange : (Value -> msg) -> Sub msg
+
+port toJs : String -> Cmd msg
