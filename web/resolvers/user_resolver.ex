76c2aac09835c0bce7832360be5efe32de0107d2
Tomas Korcak <korczis@gmail.com>
2017-07-26 18:41:37 +0200

    Rename to TheScratch

--- web/resolvers/user_resolver.ex
+++ web/resolvers/user_resolver.ex
@@ -2,7 +2,12 @@ defmodule TheScratch.UserResolver do
   alias TheScratch.Repo
   alias TheScratch.User
 
-  def all(_args, _info) do
+  require Logger
+
+  def all(args, info) do
+    Logger.info inspect(args)
+    Logger.info inspect(info)
+
     {:ok, Repo.all(User)}
   end
 end
