de945254823913b770f9179c6d50995fcc704f26
Tomas Korcak <korczis@gmail.com>
2017-08-14 20:20:46 +0200

    Make Component/Navbar View function more "functional"

--- web/elm/Component/Navbar/View.elm
+++ web/elm/Component/Navbar/View.elm
@@ -20,52 +20,76 @@ import Assets
 import Component.Application.Model exposing(Model)
 import Component.Application.Msg as Msg
 import Component.Stats.View
+import Data.User as User exposing (User)
 
 view : Model -> Html Msg.Msg
 view model =
-    let
-        signout = case model.session.user of
-            Just user ->
-                [ Navbar.formItem [ action "/auth/signout", method "post" ]
-                    [  Button.linkButton [ Button.attrs [ href "/#/profile", style[ ("padding-right", "10px") ] ] ] [ text user.email ]
-                    , Button.button [ Button.attrs [ onClick Msg.SignOut, href "javascript:void(0);" ] ] [ text "Sign out" ]
-                    , input [ Html.Attributes.attribute "type" "hidden", name "_csrf_token", value model.flags.csrf ] []
-                    , Component.Stats.View.view
-                    ]
-                ]
-            Nothing ->
-                [ Navbar.formItem [ ]
-                    [ Button.linkButton [ Button.attrs [ href "/#/auth/signin" ] ] [ text "Sign in" ]
-                    , Component.Stats.View.view
-                    ]
-                ]
-
-        navbarItems = case model.session.user of
-            Just _ ->
-                [ Navbar.itemLink [ href "/#/map" ] [ text "Map" ]
-                ]
-            Nothing ->
-                []
-
-        customItems = signout
-    in
-        div []
-            [ Navbar.config Msg.NavbarMsg
-                |> Navbar.withAnimation
-                |> Navbar.brand
-                    [ href "/#/" ]
-                    [ img
-                        [ src (Assets.path <| Assets.net)
-                        , class "d-inline-block"
-                        , style
-                            [ ( "width", "30px" )
-                            , ( "margin-right", "15px" )
-                            ]
+    div []
+        [ Navbar.config Msg.NavbarMsg
+            |> Navbar.withAnimation
+            |> Navbar.brand
+                [ href "/#/" ]
+                [ img
+                    [ src (Assets.path <| Assets.net)
+                    , class "d-inline-block"
+                    , style
+                        [ ( "width", "30px" )
+                        , ( "margin-right", "15px" )
                         ]
-                        []
-                    , text "The Scratch"
                     ]
-                |> Navbar.items navbarItems
-                |> Navbar.customItems customItems
-                |> Navbar.view model.navbar.state
-            ]
+                    []
+                , text "The Scratch"
+                ]
+            |> Navbar.items (navbarItems model.session.user)
+            |> Navbar.customItems (customItems model.session.user model.flags.csrf)
+            |> Navbar.view model.navbar.state
+        ]
+
+
+viewStats : Html msg
+viewStats =
+    Component.Stats.View.view "stats"
+
+customItems : Maybe User -> String -> List (Navbar.CustomItem Msg.Msg)
+customItems user csrf =
+    case user of
+        Just user ->
+            customItemsSignedIn user csrf
+        Nothing ->
+            customItemsSignedOut
+
+customItemsSignedIn : User -> String -> List (Navbar.CustomItem Msg.Msg)
+customItemsSignedIn user csrf =
+    [ Navbar.formItem [ action "/auth/signout", method "post" ]
+        [  Button.linkButton [ Button.attrs [ href "/#/profile", style[ ("padding-right", "10px") ] ] ] [ text user.email ]
+        , Button.button [ Button.attrs [ onClick Msg.SignOut, href "javascript:void(0);" ] ] [ text "Sign out" ]
+        , input [ Html.Attributes.attribute "type" "hidden", name "_csrf_token", value csrf ] []
+        , viewStats
+        ]
+    ]
+
+customItemsSignedOut : List (Navbar.CustomItem Msg.Msg)
+customItemsSignedOut =
+    [ Navbar.formItem [ ]
+        [ Button.linkButton [ Button.attrs [ href "/#/auth/signin" ] ] [ text "Sign in" ]
+        , viewStats
+        ]
+    ]
+
+navbarItems : Maybe User -> List (Navbar.Item msg)
+navbarItems user =
+    case user of
+        Just _ ->
+            navbarItemsSignedIn
+        Nothing ->
+            navbarItemsSignedOut
+
+navbarItemsSignedIn : List (Navbar.Item msg)
+navbarItemsSignedIn =
+    [ Navbar.itemLink [ href "/#/map" ] [ text "Map" ]
+    , Navbar.itemLink [ href "/#/graphql" ] [ text "GraphQL" ]
+    ]
+
+navbarItemsSignedOut : List (Navbar.Item msg)
+navbarItemsSignedOut =
+    []
