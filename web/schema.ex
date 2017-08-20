48e3c47b2b731c68bf4fa597c9fc282d34b1b160
Tomas Korcak <korczis@gmail.com>
2017-08-20 12:20:16 +0200

    FindUserById query uses variable - id

--- web/schema.ex
+++ web/schema.ex
@@ -4,14 +4,19 @@ defmodule TheScratch.Schema do
   """
 
   alias TheScratch.UserResolver
+  alias TheScratch.UsersResolver
 
   use Absinthe.Schema
   import_types TheScratch.Schema.Types
 
   query do
+    field :user, type: :user do
+      arg :id, non_null(:id)
+      resolve &UserResolver.find/2
+    end
+
     field :users, list_of(:user) do
-      resolve &UserResolver.all/2
+      resolve &UsersResolver.all/2
     end
   end
-
 end
