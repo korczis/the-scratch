134667340b17b84e37f7f394de015e640bddfc69
Tomas Korcak <korczis@gmail.com>
2017-08-21 01:15:03 +0200

    Test get function

--- test/lib/vfs_test.exs
+++ test/lib/vfs_test.exs
@@ -1,10 +1,17 @@
 defmodule TheScratch.VfsTest do
   use TheScratch.ConnCase
 
+  test "get" do
+    content_vfs = TheScratch.Vfs.get("graphql/user")
+    {:ok, content_fs} = File.read("web/graphql/user.graphql")
+
+    assert content_vfs == {"graphql/user", content_fs}
+  end
+
   test "get_value!" do
     content_vfs = TheScratch.Vfs.get_value!("graphql/user")
-    {:ok, content_file} = File.read("web/graphql/user.graphql")
+    {:ok, content_fs} = File.read("web/graphql/user.graphql")
 
-    assert content_vfs == content_file
+    assert content_vfs == content_fs
   end
 end
